f9200e29364f5664cbf1d6b0b0be526c68c3bcc7
Geoff Kaufman <geoff.kaufman2@gmail.com>
2024-01-03 16:05:27 -0800

    Update pre_run_tasks.py

--- pre_run_tasks.py
+++ pre_run_tasks.py
@@ -1,4 +1,5 @@
 import os
 
-with open('google-key.json', 'w') as f:
-  f.write(os.environ['DBT_USER_JSON'])
+if __name__ == "__main__":
+  with open('google-key.json', 'w') as f:
+    f.write(os.environ['DBT_USER_JSON'])
