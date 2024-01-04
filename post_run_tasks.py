0bcb91f5d8d50476786bc2e560a93abe1a3deec5
Geoff Kaufman <geoff.kaufman2@gmail.com>
2024-01-03 16:04:42 -0800

    Update post_run_tasks.py

--- post_run_tasks.py
+++ post_run_tasks.py
@@ -1,3 +1,4 @@
+import glob
 from google.cloud import storage
 import os
 
@@ -18,4 +19,17 @@ def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
         for prefix in blobs.prefixes:
             print(prefix)
 
-list_blobs_with_prefix('gk_dbt_artifacts', os.environ['BUILD_ID'])
+def upload_from_directory(directory_path: str, dest_bucket_name: str, dest_blob_name: str):
+    storage_client = storage.Client()
+    rel_paths = glob.glob(directory_path + '/**', recursive=True)
+    bucket = storage_client.get_bucket(dest_bucket_name)
+    for local_file in rel_paths:
+        remote_path = f'{dest_blob_name}/{"/".join(local_file.split(os.sep)[1:])}'
+        if os.path.isfile(local_file):
+            blob = bucket.blob(remote_path)
+            blob.upload_from_filename(local_file)
+
+
+if __name__ == "__main__":
+    upload_from_directory('target', 'gk_dbt_artifacts', os.environ['BUILD_ID'])
+    list_blobs_with_prefix('gk_dbt_artifacts', os.environ['BUILD_ID'])
