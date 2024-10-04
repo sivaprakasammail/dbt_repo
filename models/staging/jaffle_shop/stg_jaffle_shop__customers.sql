766c55509b2dbbab0a2ef2fb5f2acdb3e37c7be3
Michal Sičák <michal.sicak@slalom.com>
2024-10-04 18:31:27 +0000

    new unit test

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -9,8 +9,13 @@ renamed as (
     select
         id as customer_id,
         first_name,
-        last_name
-
+        last_name,
+        upper(left(first_name, 1))
+        || lower(right(first_name, len(first_name) - 1))
+        || ' '
+        || upper(left(last_name, 1))
+        || lower(right(last_name, len(last_name) - 1))
+            as full_name
     from source
 
 )
